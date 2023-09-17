12c677992c1399f61150618db6cd7aea2dd154ba
jonas.schrage <jonasschrage12@gmail.com>
2023-09-17 21:38:46 +0200

    Updated to preallocate arrays.

--- Matlab/insert_intervals.m
+++ Matlab/insert_intervals.m
@@ -2,39 +2,43 @@ function [new_intervals,new_indices] = insert_intervals(intervals,root_indices,n
 %INSERT_INTERVALS Summary of this function goes here
 %   Detailed explanation goes here
 arguments
-intervals;
-root_indices;
-num_roots;
+    intervals;
+    root_indices;
+    num_roots;
 end
 step = 10;
 if numel(intervals) == 2 && all(intervals == [-inf,inf])
     new_intervals = [-inf,-100,100,inf];
     new_indices = [2,3];
 else
+    max_size = sum(num_roots);
     new_intervals = intervals;
-    new_indices = [];
+    new_indices = zeros(1,max_size);
 
-    [sorted_indices,order] = sort(root_indices,'descend');
-    sorted_num_roots = num_roots(order);
-
-    for i = 1:numel(root_indices)
-        index = sorted_indices(i);
-        parts = sorted_num_roots(i)+1;
+    for i = numel(root_indices):-1:1
+        if i ~=1
+            ind_before = sum(num_roots(1:i-1));
+        else
+            ind_before = 0;
+        end
+   
+        index = root_indices(i);
+        parts = num_roots(i)+1;
         if intervals(index) == -inf
             new_intervals = [-inf,new_intervals(2)-step,new_intervals(2:end)];
-            new_indices = [new_indices+1,2]; %consider added
+            insertions = 1;
         elseif intervals(index+1) == inf
             new_intervals = [new_intervals(1:end-1),new_intervals(end-1)+step,inf];
-            new_indices = [new_indices,index+1]; %consider added
+            insertions = 1;
         else
             step_length = (intervals(index+1) - intervals(index))/parts;
             in_intervals = intervals(index)+step_length:step_length:intervals(index+1)-step_length;
             new_intervals = [new_intervals(1:index),in_intervals,new_intervals(index+1:end)];
-            new_indices = [new_indices+numel(in_intervals),index+1:index+numel(in_intervals)]; %consider added
+            insertions = num_roots(i);
         end
+        new_indices(ind_before+1:ind_before+insertions) = root_indices(i)+1+ind_before:root_indices(i)+insertions+ind_before;
     end
+    new_indices = new_indices(new_indices~=0);
 end
-new_indices = sort(new_indices);
-
 end
 
