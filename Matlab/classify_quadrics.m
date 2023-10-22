789feae93581e416131ad8bb70a13bf618b45169
jonas.schrage <jonasschrage12@gmail.com>
2023-10-22 15:46:22 +0200

    Redo classification

--- Matlab/classify_quadrics.m
+++ Matlab/classify_quadrics.m
@@ -2,10 +2,10 @@ function [result] = classify_quadrics(eigs,d0,d1,r)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 arguments
-eigs (3,1)
-d0 (1,1) {mustBeReal}
-d1 (1,1) {mustBeReal}
-r (1,1) {mustBeReal}
+    eigs (3,1)
+    d0 (1,1) {mustBeReal}
+    d1 (1,1) {mustBeReal}
+    r (1,1) {mustBeReal}
 end
 pos_eig = eigs{1};
 neg_eig = eigs{2};
@@ -41,48 +41,44 @@ switch zero_eig
             error("r should be <= 0")
         end
     case 1
-        if r < 0
-            switch pos_eig
-                case {2,0}
-                    fprintf("ell cylinder\n")
-                    result = 8;
-                case 1
-                    fprintf("hyp cylinder\n")
-                    result = 10;
-                otherwise
-                    error("Equation contains no real solutions.")
-            end
-        elseif r == 0
-            switch pos_eig
-                case 2
-                    if d0 == 0
+        if d0 == 0
+            if r < 0
+                switch pos_eig
+                    case {2,0}
+                        fprintf("ell cylinder\n")
+                        result = 8;
+                    case 1
+                        fprintf("hyp cylinder\n")
+                        result = 10;
+                    otherwise
+                        error("Equation contains no real solutions.")
+                end
+            elseif r == 0
+                switch pos_eig
+                    case {2,0}
                         fprintf("line\n")
                         result = 9;
-                    else
-                        fprintf("ell paraboloid\n")
-                        result = 6;
-                    end
-                case 1
-                    if d0 == 0
+                    case 1
                         fprintf("two planes crossing\n")
                         result = 14;
-                    else
-                        fprintf("hyp paraboloid\n")
-                        result = 7;
-                    end
-                case 0
-                    if d0 == 0
-                        fprintf("line\n")
-                        result = 9;
-                    else
-                        fprintf("ell paraboloid\n")
-                        result = 6;
-                    end
+                    otherwise
+                        error("Equation contains no real solutions.")
+                end
+            else
+                error("r should be <= 0")
+            end
+        else
+            switch pos_eig
+                case 2
+                    fprintf("ell paraboloid\n")
+                    result = 6;
+
+                case 1
+                    fprintf("hyp paraboloid\n")
+                    result = 7;
                 otherwise
                     error("Equation contains no real solutions.")
             end
-        else
-            error("r should be <= 0")
         end
     case 2
         if r == 0
