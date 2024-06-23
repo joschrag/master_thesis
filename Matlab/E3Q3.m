4ae95c21c2c827005340e2471df76a7d925027cb
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2024-06-23 16:34:31 +0200

    Move Matrix splitting into own function and fix prints.

--- Matlab/E3Q3.m
+++ Matlab/E3Q3.m
@@ -6,6 +6,7 @@ arguments
         1,2,1,1,1,0,1,1,1,-10;...
         1,1,3,0,1,-1,7,0,0,-10;...
         1,1,1,0,0,1,-15,0,0,-10;...
+        % 1,-1,1,0,0,0,-15,0,0,-4.8;...
         ];
 end
 clc;
@@ -14,55 +15,12 @@ y = sym("y","real");
 z = sym("z","real");
 vars = [x,y,z];
 v = [x^2,y^2,z^2,x*y,x*z,y*z,x,y,z,1]';
-A = -[c(:,2),c(:,3),c(:,6)];
-B = -[c(:,1),c(:,3),c(:,5)];
-C = -[c(:,1),c(:,2),c(:,4)];
 [m,~] = size(c);
-r_A = rank(A);
-r_B = rank(B);
-r_C = rank(C);
-if any([r_A,r_B,r_C] == 3)
-    if m == 3
-        if r_A == 3
-            fprintf("Using P(x)\n")
-            Q = A;
-            P = [c(:,4).*x+c(:,8),c(:,5).*x+c(:,9),c(:,1).*x^2+c(:,7).*x+c(:,10)];
-            lin_vars = [y;z;1];
-            p_var = x;
-        elseif r_B == 3
-            fprintf("Using P(y)\n")
-            Q = B;
-            P = [c(:,4).*y+c(:,7),c(:,6).*y+c(:,9),c(:,2).*y^2+c(:,8).*y+c(:,10)];
-            lin_vars = [x;z;1];
-            p_var = y;
-        elseif r_C == 3
-            fprintf("Using P(z)\n")
-            Q = C;
-            P = [c(:,5).*z+c(:,7),c(:,6).*z+c(:,8),c(:,3).*z^2+c(:,9).*z+c(:,10)];
-            lin_vars = [x;y;1];
-            p_var = z;
-        end
-        P2 = Q\P;
-    else
-        fprintf("Using MP-inverse\n")
-        P_X = [c(:,4).*x+c(:,8),c(:,5).*x+c(:,9),c(:,1).*x^2+c(:,7).*x+c(:,10)];
-        P2 = pinv(A)*P_X; %idk
-        lin_vars = [y;z;1];
-        p_var = x;
-        pinv(A)*A
-    end
-else
-    fprintf("Using MP-inverse\n")
-    P_X = [c(:,4).*x+c(:,8),c(:,5).*x+c(:,9),c(:,1).*x^2+c(:,7).*x+c(:,10)];
-    P2 = pinv(A)*P_X; %idk
-    lin_vars = [y;z;1];
-    p_var = x;
-    pinv(A)*A
-end
+[lin_vars, p_var, P2] = split_matrices(c,m, x, y, z);
 
-quad_1 = P2(1,:)*lin_vars
-quad_2 = P2(2,:)*lin_vars
-mixed = P2(3,:)*lin_vars
+quad_1 = P2(1,:)*lin_vars;
+quad_2 = P2(2,:)*lin_vars;
+mixed = P2(3,:)*lin_vars;
 
 identities = [(quad_1)*lin_vars(2) == (mixed)*lin_vars(1);...
     (mixed)*lin_vars(2) == (quad_2)*lin_vars(1);...
@@ -73,14 +31,11 @@ old_vars = [lin_vars(1)^2,lin_vars(2)^2,lin_vars(1)*lin_vars(2)];
 new_vars = [quad_1,quad_2,mixed];
 sub2 = subs(sub1,old_vars,new_vars);
 sub3 = subs(expand(sub2),old_vars,new_vars);
-vpa(collect(simplify(lhs(sub3)-rhs(sub3)),[x,y,z] ),4)
 [A_,b] = equationsToMatrix(sub3,lin_vars(1:2));
 A2 = [A_,-b];
-vpa(collect(A2))
 p = det(A2);
 coef = coeffs(p,p_var,"All");
-vpa(coef)
-p_root = sturm(eval(coef))
+p_root = sturm(double(coef),50000,10^-40);
 idx = [find(vars==p_var),find(vars==lin_vars(1)),find(vars==lin_vars(2))];
 if size(p_root,1) == 0
     plot_and_color(c)
@@ -89,8 +44,9 @@ end
 result = zeros(numel(p_root),3);
 result(:,idx(1)) = p_root;
 for i =1:numel(p_root)
-    M = subs(A2,p_var,p_root(i))
-    [~,S,V] = svd(M)
+    M = subs(A2,p_var,p_root(i));
+    %vpa(subs(det(A2),x,p_root(i)))
+    [~,S,V] = svd(M);
     [~,index] = min(diag(S));
     result(i,idx(2)) = V(1,index)/V(3,index);
     result(i,idx(3)) = V(2,index)/V(3,index);
@@ -116,11 +72,13 @@ plot_and_color(c)
 axis(horzcat(ex_x_roots,ex_y_roots,ex_z_roots))
 n = zeros(numel(p_root),1);
 for i=1:size(result,1)
-    n(i) = norm(eval(subs(c*v,[x,y,z],result(i,:))));
+    n(i) = norm(vpa(subs(c*v,[x,y,z],result(i,:))));
     eval_str = join(repmat("\n%g",[1,m]),"");
     line_break = join(repmat("=",[1,10]),"");
-    result_str = sprintf("Lösung %%i -> \nNorm: %%g%s\n%%s\n",eval_str);
-    fprintf(result_str,i,n(i),eval(subs(c*v,[x,y,z],result(i,:))),line_break);
+    res_str = join(repmat("%g",[1,m]),",");
+    res_str = join(["(x,y,z) = (",res_str,")"]);
+    result_str = sprintf("Lösung %%i\n%s-> \nNorm: %%g%s\n%%s\n",res_str,eval_str);
+    fprintf(result_str,i,result(i,:),n(i),vpa(subs(c*v,[x,y,z],result(i,:))),line_break);
 end
 end
 
