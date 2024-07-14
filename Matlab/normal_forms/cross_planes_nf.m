fc513691c7faa0647df4b476dbe7f6c00ff12e42
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2024-07-14 23:15:06 +0200

    Fix edge cases with negative eigenvalues.

--- Matlab/normal_forms/cross_planes_nf.m
+++ Matlab/normal_forms/cross_planes_nf.m
@@ -8,7 +8,7 @@ n_idx = [n_idx_p(n_idx_p~=0),n_idx_m(n_idx_m~=0),n_idx_z(n_idx_z~=0)];
 eig_vals = eig_vals(n_idx);
 P = P(:,n_idx);
 offsets = offsets(n_idx);
-tmp = 1./sqrt(eig_vals(1:2));
+tmp = 1./sqrt(abs(eig_vals(1:2)));
 params = get_cross_plane_params(tmp(1),tmp(2),P,offsets);
 end
 
--- Matlab/normal_forms/ell_cone_nf.m
+++ Matlab/normal_forms/ell_cone_nf.m
@@ -4,10 +4,15 @@ function params = ell_cone_nf(eig_vals,V,offsets)
 n_idx_p = (1:3).*(eig_vals>0)';
 n_idx_m = (1:3).*(eig_vals<0)';
 n_idx = [n_idx_p(n_idx_p~=0),n_idx_m(n_idx_m~=0)];
-eig_vals = eig_vals(n_idx);
+factor = 1;
+if sum(n_idx_m~=0) == 2
+    n_idx = [n_idx_m(n_idx_m~=0),n_idx_p(n_idx_p~=0)];
+    factor = -1;
+end
+eig_vals = factor.*eig_vals(n_idx);
 tmp = 1./sqrt(abs(eig_vals));
-V = V(:,n_idx);
-offsets = offsets(n_idx);
+V = factor.*V(:,n_idx);
+offsets = factor.*offsets(n_idx);
 params = get_ell_cone_params(tmp(1),tmp(2),tmp(3),V,offsets);
 end
 
