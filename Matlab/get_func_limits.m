48e56f09e2a596ecf196563a26782c561634cf3d
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2024-07-14 23:23:09 +0200

    Vectorize function.

--- Matlab/get_func_limits.m
+++ Matlab/get_func_limits.m
@@ -1,8 +1,14 @@
 function [limits] = get_func_limits(p)
-p = p(find(p~=0,1):end);
-is_even_degree = logical(mod(numel(p),2)) + 1;
-p_sign = sign(p(1));
-
-limits = [(-1)^(is_even_degree)*p_sign,p_sign];
-
+arguments
+    p (:,:) {mustBeReal};
+end
+[n,m] = size(p);
+c = zeros(n,1);
+s = zeros(n,1);
+for i=1:n
+c(i) = find(p(i,:)~=0,1,"first");
+s(i) = sign(p(i,c(i)));
+end
+is_even_degree = logical(mod(m-c,2));
+limits = [(-1).^(is_even_degree).*s,s]';
 end
