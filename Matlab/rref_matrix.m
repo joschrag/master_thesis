6559348bda868d84fce88ddf75039b65eb1b2b3f
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2024-08-09 23:36:24 +0200

    Add function to create a random rref form matrix.

---
+++ Matlab/rref_matrix.m
@@ -0,0 +1,30 @@
+function [M,D] = rref_matrix(ind_cols,prime,elements)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+arguments
+    ind_cols (1,:) {mustBeReal,mustBeVector}
+    prime    (1,1) {mustBeInteger,mustBeNonnegative} = 0
+    elements (1,:) {mustBeInteger,mustBePositive,mustBeVector} = []
+end
+dep_cols = setdiff(1:5,ind_cols);
+M = zeros(numel(ind_cols),5);
+if prime ~= 0
+    rand_func =@(sz) randi(prime,1,sz)-1;
+else
+    rand_func =@(sz) 10.*rand(1,sz)-5;
+end
+
+for d=dep_cols
+    sz = sum(ind_cols<d);
+    if sz > 0
+        if ~isempty(elements)
+            M(1:sz,d) = elements(1:sz);
+            elements = elements(sz:end);
+        else
+            M(1:sz,d) = rand_func(sz);
+        end
+    end
+end
+M(:,ind_cols) = eye(numel(ind_cols));
+D = M(:,dep_cols);
+end
