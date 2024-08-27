5faf721f5ba8ad8309e5799526101966e4062414
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2024-08-27 10:31:23 +0200

    Sort roots and convert results to double

--- Matlab/src/rank_cases/rank1_2.m
+++ Matlab/src/rank_cases/rank1_2.m
@@ -4,11 +4,18 @@ arguments
 end
 u = sym("u","real");
 u_0 = roots([-4*r(1,2),r(1,3)^2-4*r(1,4)]);
-u_0 = linspace(u_0,u_0-sign(r(1,2))*5,50)';
+u_0 = sort(u_0(imag(u_0)==0));
+if numel(u_0) == 2
+    if r(1,1) > 0
+        u_0 = linspace(u_0(1),u_0(2),50);
+    else
+        u_0 = [linspace(u_0(1)-2,u_0(1),25);linspace(u_0(2),u_0(2)+2,25)];
+    end
+end
 v_0 = roots([-1,-r(1,3),-r(1,2)*u-r(1,4)]);
 
 v_sol = subs(v_0,u,u_0);
-v_sol = reshape(v_sol',[numel(v_sol),1]);
-u_sol = [u_0;u_0];
+v_sol = vpa(real(reshape(v_sol',[numel(v_sol),1])));
+u_sol = vpa(real([u_0;u_0]));
 
 end
