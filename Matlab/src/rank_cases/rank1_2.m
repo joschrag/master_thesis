cf2e01ee8fe969a0e85decc7693cbac0e270ded6
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2024-09-04 18:54:59 +0200

    Add empty return if no solutions are found

--- Matlab/src/rank_cases/rank1_2.m
+++ Matlab/src/rank_cases/rank1_2.m
@@ -4,11 +4,17 @@ arguments
 end
 u = sym("u","real");
 u_0 = roots([-4*r(1,2),r(1,3)^2-4*r(1,4)]);
-u_0 = linspace(u_0,u_0-sign(r(1,2))*5,50);
+if ~isempty(u_0)
+    u_0 = linspace(u_0,u_0-sign(r(1,2))*5,50);
 v_0 = roots([-1,-r(1,3),-r(1,2)*u-r(1,4)]);
 
 v_sol = subs(v_0,u,u_0);
 v_sol = vpa(real(reshape(v_sol',[numel(v_sol),1])));
 u_sol = vpa(real(repmat(u_0,1,2)))';
+else
+    u_sol = [];
+    v_sol = [];
+end
+
 
 end
