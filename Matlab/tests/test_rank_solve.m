54103dee72acc971ffb4c60b1ef48564110b5473
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2024-09-14 21:24:41 +0200

    Fix function names

--- Matlab/tests/test_rank_solve.m
+++ Matlab/tests/test_rank_solve.m
@@ -20,7 +20,7 @@ classdef test_rank_solve < matlab.unittest.TestCase
                 c=1;
                 [m,r] = rref_matrix(c);
                 M = [m;zeros(5-numel(c),5)];
-                [t,u] = rank1_1(r);
+                [t,u] = rank1_5C3_1(r);
                 t = reshape(t,[],1);
                 u = reshape(u,[],1);
                 if ~isempty(t)
@@ -50,7 +50,7 @@ classdef test_rank_solve < matlab.unittest.TestCase
                 c=2;
                 [m,r] = rref_matrix(c);
                 M = [m;zeros(5-numel(c),5)];
-                [u,v] = rank1_2(r);
+                [u,v] = rank1_5C3_2(r);
                 if ~isempty(u)
                     for vec = [u,v]'
                         res = M*[vec(1)^2;vec(2)^2;vec(1);vec(2);1];
@@ -71,7 +71,7 @@ classdef test_rank_solve < matlab.unittest.TestCase
                 c=3;
                 [m,r] = rref_matrix(c);
                 M = [m;zeros(5-numel(c),5)];
-                [u,v] = rank1_3(r);
+                [u,v] = rank1_5C3_3(r);
                 if ~isempty(u)
                     for vec = [u,v]'
                         res = M*[vec(1)^2;vec(2)^2;vec(1);vec(2);1];
@@ -93,7 +93,7 @@ classdef test_rank_solve < matlab.unittest.TestCase
                 c=4;
                 [m,r] = rref_matrix(c);
                 M = [m;zeros(5-numel(c),5)];
-                [u,v] = rank1_4(r);
+                [u,v] = rank1_5C3_4(r);
                 if ~isempty(u)
                     for vec = [u,v]'
                         res = M*[vec(1)^2;vec(2)^2;vec(1);vec(2);1];
@@ -115,7 +115,7 @@ classdef test_rank_solve < matlab.unittest.TestCase
                 c=[1,2];
                 [m,r] = rref_matrix(c);
                 M = [m;zeros(5-numel(c),5)];
-                [u,v] = rank2_12(r);
+                [u,v] = rank2_5C3_12(r);
                 if ~isempty(u)
                     for vec = [u,v]'
                         res = M*[vec(1)^2;vec(2)^2;vec(1);vec(2);1];
@@ -130,7 +130,7 @@ classdef test_rank_solve < matlab.unittest.TestCase
                 c=[1,3];
                 [m,r] = rref_matrix(c);
                 M = [m;zeros(5-numel(c),5)];
-                [u,v] = rank2_13(r);
+                [u,v] = rank2_5C3_13(r);
                 if ~isempty(u)
                     for vec = [u,v]'
                         res = M*[vec(1)^2;vec(2)^2;vec(1);vec(2);1];
@@ -145,7 +145,7 @@ classdef test_rank_solve < matlab.unittest.TestCase
                 c=[1,4];
                 [m,r] = rref_matrix(c);
                 M = [m;zeros(5-numel(c),5)];
-                [u,v] = rank2_14(r);
+                [u,v] = rank2_5C3_14(r);
                 if ~isempty(u)
                     for vec = [u,v]'
                         res = M*[vec(1)^2;vec(2)^2;vec(1);vec(2);1];
@@ -160,7 +160,7 @@ classdef test_rank_solve < matlab.unittest.TestCase
                 c=[2,3];
                 [m,r] = rref_matrix(c);
                 M = [m;zeros(5-numel(c),5)];
-                [u,v] = rank2_23(r);
+                [u,v] = rank2_5C3_23(r);
                 if ~isempty(u)
                     for vec = [u,v]'
                         res = M*[vec(1)^2;vec(2)^2;vec(1);vec(2);1];
@@ -175,7 +175,7 @@ classdef test_rank_solve < matlab.unittest.TestCase
                 c=[2,4];
                 [m,r] = rref_matrix(c);
                 M = [m;zeros(5-numel(c),5)];
-                [u,v] = rank2_24(r);
+                [u,v] = rank2_5C3_24(r);
                 if ~isempty(u)
                     for vec = [u,v]'
                         res = M*[vec(1)^2;vec(2)^2;vec(1);vec(2);1];
@@ -190,7 +190,7 @@ classdef test_rank_solve < matlab.unittest.TestCase
                 c=[3,4];
                 [m,r] = rref_matrix(c);
                 M = [m;zeros(5-numel(c),5)];
-                [u,v] = rank2_34(r);
+                [u,v] = rank2_5C3_34(r);
                 if ~isempty(u)
                     for vec = [u,v]'
                         res = M*[vec(1)^2;vec(2)^2;vec(1);vec(2);1];
@@ -205,7 +205,7 @@ classdef test_rank_solve < matlab.unittest.TestCase
                 c=[1,2,3];
                 [m,r] = rref_matrix(c);
                 M = [m;zeros(5-numel(c),5)];
-                [u,v] = rank3_123(r);
+                [u,v] = rank3_5C3_123(r);
                 if ~isempty(u)
                     for vec = [u,v]'
                         res = M*[vec(1)^2;vec(2)^2;vec(1);vec(2);1];
@@ -220,7 +220,7 @@ classdef test_rank_solve < matlab.unittest.TestCase
                 c=[1,2,4];
                 [m,r] = rref_matrix(c);
                 M = [m;zeros(5-numel(c),5)];
-                [u,v] = rank3_124(r);
+                [u,v] = rank3_5C3_124(r);
                 if ~isempty(u)
                     for vec = [u,v]'
                         res = M*[vec(1)^2;vec(2)^2;vec(1);vec(2);1];
@@ -241,7 +241,7 @@ classdef test_rank_solve < matlab.unittest.TestCase
                 c=[1,3,4];
                 [m,r] = rref_matrix(c);
                 M = [m;zeros(5-numel(c),5)];
-                [u,v] = rank3_134(r);
+                [u,v] = rank3_5C3_134(r);
                 if ~isempty(u)
                     for vec = [u,v]'
                         res = M*[vec(1)^2;vec(2)^2;vec(1);vec(2);1];
@@ -262,7 +262,7 @@ classdef test_rank_solve < matlab.unittest.TestCase
                 c=[2,3,4];
                 [m,r] = rref_matrix(c);
                 M = [m;zeros(5-numel(c),5)];
-                [u,v] = rank3_234(r);
+                [u,v] = rank3_5C3_234(r);
                 if ~isempty(u)
                     for vec = [u,v]'
                         res = M*[vec(1)^2;vec(2)^2;vec(1);vec(2);1];
@@ -284,7 +284,7 @@ classdef test_rank_solve < matlab.unittest.TestCase
                 c=[1,2,3,4];
                 [m,r] = rref_matrix(c);
                 M = [m;zeros(5-numel(c),5)];
-                [u,v] = rank4_1234(r);
+                [u,v] = rank4_5C3_1234(r);
                 if ~isempty(u)
                     for vec = [u,v]'
                         res = M*[vec(1)^2;vec(2)^2;vec(1);vec(2);1];
