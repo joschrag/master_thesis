119add8e79676c777c5f61cc44af78ae4382926d
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2024-09-15 19:37:14 +0200

    Add execution mode

--- Matlab/rref_matrix.m
+++ Matlab/rref_matrix.m
@@ -1,15 +1,19 @@
-function [M,D] = rref_matrix(ind_cols,prime,elements)
+function [M,D] = rref_matrix(ind_cols,len,prime,elements,opt)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 arguments
     ind_cols (1,:) {mustBeReal,mustBeVector}
+    len      (1,1) {mustBeInteger,mustBePositive}
     prime    (1,1) {mustBeInteger,mustBeNonnegative} = 0
     elements (1,:) {mustBeInteger,mustBePositive,mustBeVector} = []
+    opt.mode (1,1) string {mustBeMember(opt.mode,["real","integer"])} = "real";
 end
-dep_cols = setdiff(1:5,ind_cols);
-M = zeros(numel(ind_cols),5);
+dep_cols = setdiff(1:len,ind_cols);
+M = zeros(numel(ind_cols),len);
 if prime ~= 0
     rand_func =@(sz) randi(prime,1,sz)-1;
+elseif opt.mode == "integer"
+    rand_func =@(sz) randi(20,1,sz)-10;
 else
     rand_func =@(sz) 10.*rand(1,sz)-5;
 end
