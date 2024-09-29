94b8fd499af4794a5e2b4edfdcb93cf5aa2c15fb
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2024-09-29 23:52:27 +0200

    Add comments and docstrings

--- Matlab/src/4C3/rank_cases/rank4_4C3_1234.m
+++ Matlab/src/4C3/rank_cases/rank4_4C3_1234.m
@@ -1,16 +1,14 @@
 function [v_sol,w_sol] = rank4_4C3_1234(r)
-%RANK4_1234 Summary of this function goes here
-%   Detailed explanation goes here
+%RANK4_4C3_1234 Solve the resulting subsystem of equations for the case R1234.
 arguments
     r (4,2) {mustBeReal}
 end
-
+% Obtain solution candidates from equations
 w_root = roots([-1,-r(3,1),-r(3,2)]);
 w_root = w_root(abs(imag(w_root))<10^-10);
 v_root = -r(4,1).*w_root-r(4,2);
-
+% Remove roots not satysfying the conditions
 conds = abs(-r(1,1).*w_root-r(1,2)-v_root.^2) < 10^-10 & abs(-r(2,1).*w_root-r(2,2)-v_root.*w_root) < 10^-10;
 v_sol = v_root(conds);
 w_sol = w_root(conds);
 end
-
--- Matlab/src/4C3/rank_cases/rank5_4C3_12345.m
+++ Matlab/src/4C3/rank_cases/rank5_4C3_12345.m
@@ -1,18 +1,15 @@
 function [v_sol,w_sol] = rank5_4C3_12345(r)
-%RANK5_12345 Summary of this function goes here
-%   Detailed explanation goes here
+%RANK5_4C3_12345 Solve the resulting subsystem of equations for the case R12345.
 arguments
     r (5,1) {mustBeReal}
 end
-
-if abs(r(1,1) + r(4,1)^2) < 10^-10 && ...
-        abs(r(3,1) + r(5,1)^2) < 10^-10 &&...
-        abs(r(2,1) + r(4,1)*r(5,1)) < 10^-10
-    v_sol = -r(4,1);
-    w_sol = -r(5,1);
-else
-    v_sol = [];
-    w_sol = [];
+% Obtain solution candidates from equations
+v_root = -r(4,1);
+w_root = -r(5,1);
+% Remove roots not satysfying the conditions
+cond =  abs(r(1,1) + r(4,1)^2) < 10^-10 &...
+    abs(r(3,1) + r(5,1)^2) < 10^-10 &...
+    abs(r(2,1) + r(4,1)*r(5,1)) < 10^-10;
+v_sol = v_root(cond);
+w_sol =  w_root(cond);
 end
-end
-
