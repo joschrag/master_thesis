7b9793fda71c24234f2d35ea151c27bf9379f89e
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2024-09-30 20:01:29 +0200

    Fix name shadowing

--- Matlab/src/5C3/split_matrices_5C3.m
+++ Matlab/src/5C3/split_matrices_5C3.m
@@ -14,32 +14,34 @@ arguments
 end
 
 assert(~any(C(:,5)))
-A = -[C(:,7),C(:,8),C(:,9),C(:,10),C(:,5)*x+C(:,15)];
-B = -[C(:,1),C(:,3),C(:,6),C(:,10),C(:,5)*y+C(:,13)];
-C = -[C(:,1),C(:,2),C(:,4),C(:,7),C(:,5)*z+C(:,12)];
-i =   [ 4,14,6,16,2,12,18,3,13,19,1,11,17;...
+p_idx =   [ 4,14,6,16,2,12,18,3,13,19,1,11,17;...
     2,11,9,16,4,12,17,8,15,19,7,14,18;...
     3,11,8,14,6,13,17,9,15,18,10,16,19];
-
-Q_list = {A,B,C};
+q_idx = [7,8,9,10,5,15;1,3,6,10,5,13;1,2,4,7,5,12];
+Q = cell(1,3);
+ranks = zeros(3,1);
+conds = nan(3,1);
+for i=1:3
+    Q{i} = -[C(:,q_idx(i,1)),C(:,q_idx(i,2)),C(:,q_idx(i,3)),...
+        C(:,q_idx(i,4)),C(:,q_idx(i,5))*x+C(:,q_idx(i,6))];
+    ranks(i) = rank(Q{i});
+    conds(i) = cond(Q{i});
+end
 v = [x;y;z];
 
-if any([rank(A),rank(B),rank(C)]==5)
-    conds = real([vpa(cond(A)),vpa(cond(B)),vpa(cond(C))]);
+if any(ranks==5)
     [~,I] = min(conds,[],"omitnan");
     if options.verbose > 0
         fprintf("Using P(%s)\n",string(v(I)))
     end
-    Q = Q_list{I};
     p_var = v(I);
-    
-    P  = [C(:,i(I,1)).*p_var+C(:,i(I,2)),...
-        C(:,i(I,3)).*p_var+C(:,i(I,4)),...
-        C(:,i(I,5)).*p_var^2+C(:,i(I,6)).*p_var+C(:,i(I,7)),...
-        C(:,i(I,8)).*p_var^2+C(:,i(I,9)).*p_var+C(:,i(I,10)),...
-        C(:,i(I,11)).*p_var^3+C(:,i(I,12)).*p_var^2+C(:,i(I,13)).*p_var+C(:,20)];
+    P  = [C(:,p_idx(I,1)).*p_var+C(:,p_idx(I,2)),...
+        C(:,p_idx(I,3)).*p_var+C(:,p_idx(I,4)),...
+        C(:,p_idx(I,5)).*p_var^2+C(:,p_idx(I,6)).*p_var+C(:,p_idx(I,7)),...
+        C(:,p_idx(I,8)).*p_var^2+C(:,p_idx(I,9)).*p_var+C(:,p_idx(I,10)),...
+        C(:,p_idx(I,11)).*p_var^3+C(:,p_idx(I,12)).*p_var^2+C(:,p_idx(I,13)).*p_var+C(:,20)];
     lin_vars = [v(setdiff(1:3,I)).^2;v(setdiff(1:3,I));1];
-    P2 = Q\P;
+    P2 = Q{I}\P;
 else
     if options.error
         error("All Matrices are singular!")
