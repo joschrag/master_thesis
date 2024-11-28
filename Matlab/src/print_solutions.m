705e4e201b9ace69ebd8049c4434a0c12a4e7ca6
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2024-11-28 11:17:18 +0100

    Pre apply mod function to eliminate rounding error.

--- Matlab/src/print_solutions.m
+++ Matlab/src/print_solutions.m
@@ -10,12 +10,12 @@ end
 n = zeros(size(result,1),1);
 m = size(eqs,1);
 for i=1:size(result,1)
-    n(i) = norm(mod(eval(subs(eqs,[x,y,z],result(i,:))),prime));
+    n(i) = norm(mod(eval(subs(mod(eqs,prime),[x,y,z],result(i,:))),prime));
     eval_str = join(repmat("\n%g",[1,m]),"");
     line_break = join(repmat("=",[1,10]),"");
     res_str = join(repmat("%g",[1,3]),",");
     res_str = join(["(",res_str,")"]);
     result_str = sprintf("Solution %%i %s-> \nNorm: %%i%s\n%%s\n",res_str,eval_str);
-    fprintf(result_str,i,result(i,:),n(i),mod(eval(subs(eqs,[x,y,z],result(i,:))),prime),line_break);
+    fprintf(result_str,i,result(i,:),n(i),mod(eval(subs(mod(eqs,prime),[x,y,z],result(i,:))),prime),line_break);
 end
 end
